4442f96d551bd54b21494424d1d52a6bf55593d0
Moinuddin-hub <moincse022@gmail.com>
2024-04-13 19:12:24 +0600

    sslcommerce intregration

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
+const SSLCommerzPayment = require("sslcommerz-lts");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -20,6 +21,13 @@ const client = new MongoClient(uri, {
   },
 });
 
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false; //true for live, false for sandbox
+// const store_id = text66195c53d689e
+// const store_passwd = text66195c53d689e@ssl
+// const is_live = false; //true for live, false for sandbox
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,63 +36,116 @@ async function run() {
     const userCollection = client.db("gasDb").collection("users");
     const reviewCollection = client.db("gasDb").collection("reviews");
     const cartCollection = client.db("gasDb").collection("carts");
+    const orderCollection = client.db("gasDb").collection("order");
     // jwt related API
-    app.post("/jwt", async(req, res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
-      }); 
+      });
       console.log("jwt token", token);
       res.send({ token });
     });
     // middleware;
-    const verifyToken= (req, res, next) => {
-      console.log("inside verify token",req.headers.authorization);
-      if(!req.headers.authorization){
-        return res.status(401).send({message: "unauthorized access"});
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
       }
-      const token=req.headers.authorization.split(" ")[1];
+      const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if(err){
-          return res.status(401).send({message: "unauthorized access"})
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
         }
         req.decoded = decoded;
-        next(); 
-      })
-    }
-// user verify admin after verifyToken
+        next();
+      });
+    };
+    // user verify admin after verifyToken
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query={email: email};
+      const query = { email: email };
       const user = await userCollection.findOne(query);
       const isAdmin = user?.role === "admin";
-      if(!isAdmin){
-        return res.status(403).send({message: "forbidden access"})
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
       }
       next();
-  
-    }
+    };
+    // payment related api
+    const tran_id=new ObjectId().toString();
+   app.post("/order", async (req, res) => {
+      const order = req.body;
+      console.log(order)
+      // const result = await cartCollection.findOne(order);
+      // console.log(result);
+      // res.send(result);
+ 
+ //sslcommerz init
 
+  const data = {
+      total_amount:order.price,
+      currency:order.currency,
+      tran_id: tran_id, // use unique tran_id for each api call
+      success_url:`http://localhost:5000/payment/success/${tran_id}`,
+      fail_url: 'http://localhost:3030/fail',
+      cancel_url: 'http://localhost:3030/cancel',
+      ipn_url: 'http://localhost:3030/ipn',
+      shipping_method: 'Courier',
+      product_name: 'Computer.',
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name:order.name,
+      cus_email: 'customer@example.com',
+      cus_add1:order.address,
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone:order.phone,
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
+  };
+  console.log(data);
+  const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+  sslcz.init(data).then(apiResponse => {
+      // Redirect the user to payment gateway
+      let GatewayPageURL = apiResponse.GatewayPageURL
+      res.send({url:GatewayPageURL});
+      console.log('Redirecting to: ', GatewayPageURL)
+  });
+  app.post('/payment/success/:tranId',async(req,res)=>{
+    console.log(req.params.tranId);
+  })
+});
+ 
     // Users related API
-    app.get("/users",verifyToken, async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
       console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result);
     });
- 
+
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
-      if( email!==req.decoded.email ){
-        return res.status(403).send({message:'forbidden-access'});
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden-access" });
       }
-      const query={email: email};
-      const user=await userCollection.findOne(query);
-      let admin=false;
-      if(user){
-        admin=user?.role==="admin";
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
       }
-      res.send({admin: admin});
-    })
+      res.send({ admin: admin });
+    });
 
     app.patch("/user/admin/:id", async (req, res) => {
       const id = req.params.id;
@@ -123,19 +184,17 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await productCollection.findOne(query);
       res.send(result);
-      
-    })
-    app.post("/product", verifyToken,verifyAdmin, async (req, res) => {
+    });
+    app.post("/product", verifyToken, verifyAdmin, async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
       res.send(result);
-      
-    })
-   app.patch("/product/:id", async (req, res) => {
-    const product = req.body;
+    });
+    app.patch("/product/:id", async (req, res) => {
+      const product = req.body;
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-    
+
       const updateDoc = {
         $set: {
           name: product.name,
@@ -143,18 +202,17 @@ async function run() {
           price: product.price,
           IssueDate: product.IssueDate,
           img: product.img,
-        }
-      }
+        },
+      };
       const result = await productCollection.updateOne(filter, updateDoc);
       res.send(result);
-   })
-    app.delete("/product/:id", verifyToken,verifyAdmin, async (req, res) => {
+    });
+    app.delete("/product/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await productCollection.deleteOne(query);
       res.send(result);
-      
-    })
+    });
     // reviews related API
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
